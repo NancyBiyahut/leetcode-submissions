7c78dd596bba80d399e6c7b833a367601c7a9861
NancyBiyahut <99260268+NancyBiyahut@users.noreply.github.com>
2022-06-27 14:15:53 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (80.01%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+     double myPow(double x, int n) 
+    {
+       if(n==0) 
+            return 1;
+        
+        double ans;
+        
+        if(n<0) { 
+            double pow = myPow(x,n/2);
+            ans = pow*pow;
+            if(n%2!=0)
+                ans/=x;
+        }
+        
+        else { // for positive
+            double pow = myPow(x,n/2);
+            ans = pow*pow;
+            if(n%2!=0)
+                ans*=x;
+        }    
+        
+        return ans;
+    }
+};
+
+
+
+/*public double myPow(double x, int n) {
+        if(n==0) // base case
+            return 1;
+        
+        double ans;
+        if(n<0) { // for negative
+            double pow = myPow(x,n/2);
+            ans = pow*pow;
+            if(n%2!=0)
+                ans/=x;
+        }
+        
+        else { // for positive
+            double pow = myPow(x,n/2);
+            ans = pow*pow;
+            if(n%2!=0)
+                ans*=x;
+        }    
+        
+        return ans;
+    }
+}
+*/
